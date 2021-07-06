019fc24d2741a411e66bf3cef95bbf287c47347e
Jeffrey Bradley <jnb2387@gmail.com>
2021-07-06 12:16:28 -0600

    first

---
+++ src/pages/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- <q-page class="flex flex-center"> -->
+  <div id="">Card</div>
+  <!-- </q-page> -->
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default {
+  name: "Card",
+  data() {
+    return {};
+  },
+  mounted: function () {},
+  methods: {},
+};
+</script>
+<style></style>
